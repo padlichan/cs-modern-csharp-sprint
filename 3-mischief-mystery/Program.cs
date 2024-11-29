31f0c2df1f2d7595b5d571dba63aecba07b514d7
Jayla (they/them) <jakeclegg99@gmail.com>
2024-11-29 13:44:50 +0000

    Added LInQ to suspects

--- 3-mischief-mystery/Program.cs
+++ 3-mischief-mystery/Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using _3_mischief_mystery;
+using Suspects = System.Collections.Generic.List<_3_mischief_mystery.Suspect>;
 
 Console.WriteLine("Hello, World!");
 
@@ -7,7 +8,33 @@ ResidentData residentData = new();
 
 residentData.NewFob = [..residentData.OldFobVisits, [1, 10, 4, 2, 1, 5], [1, 10, 8, 1, 3]];
 
-int[] culpritFlatNumbers = residentData.NewFob.Where(x => x is [_, 10, >= 3, .., 1, < 5]).Select((x, i) => i).ToArray();
+List<int> suspectFlatNumbers = residentData.NewFob.Where(x => x is [_, 10, >= 3, .., 1, < 5]).Select((x, i) => i).ToList();
 
-Console.WriteLine(culpritFlatNumbers.Length);
+Console.WriteLine(suspectFlatNumbers.Count);
+
+//Get the Residents from ResidentData
+//CHekc apartment num
+// if same create suspect obj
+
+Suspects suspects = new Suspects();
+
+Suspects linqSuspects = residentData.Residents
+	.Where(r => suspectFlatNumbers.Contains(r.apartmentNumber))
+	.Select(r => new Suspect(r.apartmentNumber,r.name, r.age,r.heightInCm.ToString(), true)).ToList();
+
+
+for (int i = 0; i < residentData.Residents.Length; i++)
+{
+	if (suspectFlatNumbers.Contains(residentData.Residents[i].apartmentNumber))
+	{
+		Resident r = residentData.Residents[i];
+		Suspect sus = new(r.apartmentNumber, r.name, r.age, r.heightInCm.ToString(), true);
+		suspects.Add(sus);
+		
+	}
+
+}
+
+
+Console.WriteLine(linqSuspects.Count);
 
---
+++ 3-mischief-mystery/Suspect.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_mischief_mystery
+{
+    internal class Suspect(int apartmentNumber, string name,int age, string height, bool stillASuspect)
+    {
+        public int ApartmentNumber { get; set; } = apartmentNumber;
+        public string Name { get; set; } = name;
+        public int Age { get; set; } = age;
+        public string Height { get; set; } = height;
+
+        public bool StillASuspect { get; set; } = stillASuspect;
+
+
+    }
+}
