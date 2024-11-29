45a97686da5e071140201813b932f5d6a0ca5be9
Lili <lili.berenyi@gmail.com>
2024-11-29 12:13:31 +0000

    Add solution to tasks 7 and 8

--- 3-mischief-mystery/Program.cs
+++ 3-mischief-mystery/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using _3_mischief_mystery;
+
 Console.WriteLine("Hello, World!");
+
+ResidentData residentData = new();
+
+residentData.NewFob = [..residentData.OldFobVisits, [1, 10, 4, 2, 1, 5], [1, 10, 8, 1, 3]];
+
+int[] culpritFlatNumbers = residentData.NewFob.Where(x => x is [_, 10, >= 3, .., 1, < 5]).Select((x, i) => i).ToArray();
+
+Console.WriteLine(culpritFlatNumbers.Length);
+
